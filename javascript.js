91760d961991680de9f85e2b4182ccf3317a64b4
Reid <50605126+iFlaffy@users.noreply.github.com>
2024-03-29 12:37:17 +1300

    Set up random num gen to determine computer choice

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
     let compChoice = Math.round(Math.random() * (3 - 1) + 1)
 
     if (compChoice === 1) {
-        return console.log("Rock")
-    } else if (i === 2) {
-        return console.log("Paper")
+        return "Rock"
+    } else if (compChoice === 2) {
+        return "Paper"
     } else {
-        return console.log("Scissors")
+        return "Scissors"
     }
 }
 
